44c7c84d019802a8a8a1d3d727e1881edd907e09
Harshikerfuffle <harshika30j@gmail.com>
2019-01-22 10:28:26 -0500

    Update 2led_blink.ino

--- 2led_blink.ino
+++ 2led_blink.ino
@@ -10,26 +10,62 @@ void setup() {
  digitalWrite(led1, LOW);
  digitalWrite(led2, LOW);
  digitalWrite(led3, LOW);
- Particle.function("led", ledControl); //Register our Particle function here
+ 
+ Particle.function("led1", ledControl1); //Register our Particle function here
+ Particle.function("led2", ledControl2);
+ Particle.function("led3", ledControl3);
 }
 
-void loop() {
-int ledControl(String command){
+void loop() 
+{
+}
+
+//--------------------------------   
+int ledControl1(String command){
    int state = LOW;
-   
+	
    if(command == "HIGH")
-   { 
-	   state = HIGH; // find out the state of the led
-   }
+   { state = HIGH;}// find out the state of the led
+	
    else if(command == "LOW")
-   { 
-     state = LOW;
-   }
+   { state = LOW;}
+	
    else
-   {
-	   return -1;
-   }
+   { return -1;}
    
    digitalWrite(led1, state); // write to the appropriate pin
    return 1;
+}
+//--------------------------------   
+int ledControl2(String command){
+  int state = LOW;
+   
+  if(command == "HIGH")
+  { state = HIGH;}// find out the state of the led
+   
+  else if(command == "LOW")
+  { state = LOW;}
+   
+  else
+  { return -1;}
+   
+  digitalWrite(led2, state);
+  return 1;
+ }
+ 
+//--------------------------------      
+int ledControl3(String command){
+  int state = LOW;
+   
+  if(command == "HIGH")
+  { state = HIGH;}// find out the state of the led
+   
+  else if(command == "LOW")
+  { state = LOW;}
+   
+  else
+  { return -1;}
+
+  digitalWrite(led3, state);
+  return 1;
 }
